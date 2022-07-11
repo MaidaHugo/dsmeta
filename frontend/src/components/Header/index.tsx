2a553ef79965b839c9ec1e4320cfaaab0b427239
MaidaHugo <hugo_maida@hotmail.com>
2022-07-11 15:14:45 -0300

    Other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import logo from "../../assets/img/logo.svg";
+
+import "./styles.css";
+function Header() {
+  return (
+    <header>
+      <div className="dsmeta-logo-container">
+        <img src={logo} alt="DSMeta" />
+        <h1>DSMeta</h1>
+        <p>
+          Desenvolvido por&nbsp;
+          <a href="https://github.com/MaidaHugo" target="_blank">
+            @MaidaHugo
+          </a>
+        </p>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
