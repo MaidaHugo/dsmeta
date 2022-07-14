ce8cc611e6469bca2ec6326d1cfce8eba25b9b5b
MaidaHugo <hugo_maida@hotmail.com>
2022-07-14 10:59:37 -0300

    style correction

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,11 +1,12 @@
 import logo from "../../assets/img/logo.svg";
 
 import "./styles.css";
+
 function Header() {
   return (
     <header>
       <div className="dsmeta-logo-container">
-        <img src={logo} alt="DSMeta" />
+        <img src={logo} alt="" />
         <h1>DSMeta</h1>
         <p>
           Desenvolvido por&nbsp;
